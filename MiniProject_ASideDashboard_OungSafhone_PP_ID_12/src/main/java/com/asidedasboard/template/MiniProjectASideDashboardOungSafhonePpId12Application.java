a0f5b84a314f3d27b0d204ebc0b9fb49d12b9ca7
SafhoneOung <oung_safhone777@hotmail.com>
2017-06-22 19:08:09 +0700

    first init

---
+++ MiniProject_ASideDashboard_OungSafhone_PP_ID_12/src/main/java/com/asidedasboard/template/MiniProjectASideDashboardOungSafhonePpId12Application.java
@@ -0,0 +1,12 @@
+package com.asidedasboard.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectASideDashboardOungSafhonePpId12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectASideDashboardOungSafhonePpId12Application.class, args);
+	}
+}
